d072fddae4694b1a11782c8dd0c53b78b8900369
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-05-13 17:52:49 +0530

    added add_to_list function

--- linkedlist.c
+++ linkedlist.c
@@ -16,3 +16,19 @@ List_ptr create_list(void)
   new_list->last = NULL;
   return new_list;
 }
+
+Status add_to_list(List_ptr list, Element data)
+{
+  Node_ptr new_node = create_node(data);
+  if (list->first == NULL)
+  {
+    list->first = new_node;
+  }
+  else
+  {
+    list->last->next = new_node;
+  }
+  list->last = new_node;
+  list->length++;
+  return Success;
+}
