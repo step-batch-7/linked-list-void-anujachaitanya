5973244239e4e11f9f7ed7a858a50dd41958e4bb
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-05-16 11:38:57 +0530

    added test for remove at for empty lis t

--- test/test_main.c
+++ test/test_main.c
@@ -174,6 +174,29 @@ void test_remove_from_start_in_the_long_list(void)
   Status result = assert_void_linked_list(expected, actual, int_equal) && assert_result(removed, &new_number, int_equal);
   show_result(result, description);
 }
+
+void test_remove_at_for_invalid_position(void)
+{
+  char description[] = "Should not remove  the element for invalid position";
+  int expected_values[0] = {};
+  int value = 3;
+  List_ptr expected = init_linked_list(expected_values, 0, 4);
+  List_ptr actual = create_list();
+  insert_at(actual, &value, 8);
+  Status result = assert_void_linked_list(actual, expected, int_equal);
+  show_result(result, description);
+}
+
+void test_remove_at_for_empty_list(void)
+{
+  char description[] = "Should not remove from empty list";
+  char expected_values[0];
+  List_ptr expected = init_linked_list(expected_values, 0, 1);
+  List_ptr actual = create_list();
+  remove_at(actual, 0);
+  Status result = assert_void_linked_list(actual, expected, char_equal);
+  show_result(result, description);
+}
 int main(void)
 {
   test_for_add_to_list_empty_list();
@@ -188,5 +211,7 @@ int main(void)
   test_remove_from_in_long_list();
   test_remove_from_start_fro_empty_list();
   test_remove_from_start_in_the_long_list();
+  test_remove_at_for_invalid_position();
+  test_remove_at_for_empty_list();
   return 0;
 }
